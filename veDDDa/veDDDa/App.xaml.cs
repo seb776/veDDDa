878e65cf0645e2982dabd2b2cac366a69621288b
seb776 <sebastien.maire@epitech.eu>
2023-02-24 00:48:25 +0100

    Started to work on the app, does not compile

---
+++ veDDDa/veDDDa/App.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace veDDDa
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public const double FRAMERATE = 60.0;
+        public const string SHADER_PATH = @"C:\path\to\folder";
+        private List<MainWindow> _windows;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            _windows = new List<MainWindow>();
+
+            MainWindow leftEyeWin = new MainWindow(EEye.LEFT);
+            _windows.Add(leftEyeWin);
+            leftEyeWin.Show();
+            MainWindow rightEyeWin = new MainWindow(EEye.RIGHT);
+            _windows.Add(rightEyeWin);
+            rightEyeWin.Show();
+
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(1000.0 / FRAMERATE)
+            };
+            float accTime = 0.0f;
+            var lastTime = DateTime.Now;
+            timer.Tick += (s, _) =>
+            {
+                var curTime = DateTime.Now;
+                float diffTime = (float)((curTime - lastTime).TotalSeconds);
+                accTime += diffTime;
+                lastTime = curTime;
+                foreach (var win in _windows)
+                {
+                    win.UpdateUniforms(accTime);
+                }
+                //_winformGLControl.Invalidate();
+                //_winformGLControl_Paint(null, null);
+            };
+
+            var watcher = new FileSystemWatcher(SHADER_PATH);
+
+            watcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+
+            watcher.Changed += Watcher_Changed;
+
+            watcher.EnableRaisingEvents = true;
+
+        }
+
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            var shaderCode = File.ReadAllText(e.FullPath);
+            foreach (var win in _windows)
+            {
+                win.UpdateShaderCode(shaderCode);
+            }
+        }
+    }
+}
---
+++ veDDDa/veDDDa/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace veDDDa
+{
+    public enum EEye
+    {
+        LEFT,
+        RIGHT
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private EEye _eye;
+
+        public EEye Eye
+        {
+            get { return _eye; }
+            private set { _eye = value; }
+        }
+        private GLControl _winformGLControl;
+
+        public MainWindow(EEye eye)
+        {
+            InitializeComponent();
+            _winformGLControl = new GLControl();
+            _winformGLControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            _winformGLControl.Paint += _winformGLControl_Paint; ;
+            winFormsHost.Child = _winformGLControl;
+            winFormsHost.Width = 800;
+            winFormsHost.Height = 600;
+        }
+
+        private void _winformGLControl_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+
+        }
+
+        public void UpdateShaderCode(string shader)
+        {
+
+        }
+        public void Build(string code)
+        {
+            int fragmentIndex = 0;
+            fragmentIndex = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentIndex, code);
+            GL.CompileShader(fragmentIndex);
+            int compileStatus = 0;
+            GL.GetShader(fragmentIndex, ShaderParameter.CompileStatus, out compileStatus);
+            if (compileStatus == 0)
+                throw new InvalidOperationException("unable to compiler vertex shader: " + GetShaderInfoLog(fragmentIndex));
+
+            _program = GL.CreateProgram();
+
+            GL.AttachShader(_program, fragmentIndex);
+
+            //int[] arr = new int[42]; // TODO useless ?
+            //GL.GetProgram(0, ProgramProperty.ActiveUniforms, arr);
+
+            GL.LinkProgram(_program);
+
+            GL.GetProgram(_program, (GetProgramParameterName)OpenTK.Graphics.OpenGL.All.LinkStatus, out compileStatus);
+            if (compileStatus == 0)
+                throw new InvalidOperationException("unable to link program");
+
+
+            GL.UseProgram(_program);
+
+            _textureALocation = GL.GetUniformLocation(_program, "textureA");
+
+        }
+        public void UpdateUniforms(float time)
+        {
+
+        }
+    }
+}
